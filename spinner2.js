8cf5e0bccc7158033a26153b3274ab69891cd3f6
Sarah Moss <sarah-moss@live.com>
2022-04-22 02:22:55 +0000

    Refactor spinner1 implementation in spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,39 +1,10 @@
 // refactored spinner1.js
 
-setTimeout(() => {
-  process.stdout.write('\r| ');
-}, 100);
+let spinnerChars = ['\r| ', '\r/ ', '\r- ', '\r\\  ', '\r| ', '\r/ ', '\r- ', '\r\\  ', '\r| '];
 
+for (let i = 0; i < spinnerChars.length; i++) {
 setTimeout(() => {
-  process.stdout.write('\r/ ');
-}, 300);
+  process.stdout.write(spinnerChars[i]);
+}, (100 + (i * 200)))
+};
 
-setTimeout(() => {
-  process.stdout.write('\r- ');
-}, 500);
-
-setTimeout(() => {
-  // escape backslash special character
-  process.stdout.write('\r\\ '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r| ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/ ');
-}, 1100)
-
-setTimeout(() => {
-  process.stdout.write('\r- ')
-}, 1300);
-
-setTimeout(() => {
-   // escape backslash special character
-  process.stdout.write('\r\\  ');
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r| ');
-}, 1700);
