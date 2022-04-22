b9f7d19efd7f10e39c7459df8d22ae528b649382
Sarah Moss <sarah-moss@live.com>
2022-04-22 02:25:19 +0000

    Eslint code

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,10 @@
 // refactored spinner1.js
 
-let spinnerChars = ['\r| ', '\r/ ', '\r- ', '\r\\  ', '\r| ', '\r/ ', '\r- ', '\r\\  ', '\r| '];
+let spinnerChars = ['\r| ', '\r/ ', '\r- ', '\r\\ ', '\r| ', '\r/ ', '\r- ', '\r\\ ', '\r| '];
 
 for (let i = 0; i < spinnerChars.length; i++) {
-setTimeout(() => {
-  process.stdout.write(spinnerChars[i]);
-}, (100 + (i * 200)))
-};
+  setTimeout(() => {
+    process.stdout.write(spinnerChars[i]);
+  }, (100 + (i * 200)));
+}
 
